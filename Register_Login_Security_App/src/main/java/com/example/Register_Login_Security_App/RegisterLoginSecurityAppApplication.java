ea78d163453868fb3cfd713b856d60f81445468c
Somnath <somnathlatthe@gmail.com>
2024-05-26 17:50:46 +0530

    initial commit

---
+++ Register_Login_Security_App/src/main/java/com/example/Register_Login_Security_App/RegisterLoginSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.Register_Login_Security_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterLoginSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterLoginSecurityAppApplication.class, args);
+	}
+
+}
---
+++ Register_Login_Security_App/src/test/java/com/example/Register_Login_Security_App/RegisterLoginSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Register_Login_Security_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterLoginSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
